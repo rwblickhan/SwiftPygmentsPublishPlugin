6fc51499758edee72fc485b306150a0b2742de32
Russell Blickhan <rwblickhan@gmail.com>
2022-06-17 10:27:52 -0700

    Bump to swift-tools-version-5.5

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftPygmentsPublishPlugin",
+    platforms: [.macOS(.v12)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -14,8 +14,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Ze0nC/SwiftPygments", .branch("master")),
-        .package(url: "https://github.com/johnsundell/publish.git", from: "0.1.0"),
+        .package(name: "SwiftPygments", url: "https://github.com/Ze0nC/SwiftPygments", .branch("master")),
+        .package(name: "Publish", url: "https://github.com/rwblickhan/publish.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
